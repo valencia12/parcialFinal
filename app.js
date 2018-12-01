ad4c04601c96e0710b65922d8be1736007ef70d4
Jorge <valencia12@uca.edu.sv>
2018-12-01 16:23:37 -0600

    se soluciono

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -5,16 +6,25 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var tareasRouter = require('./routes/tareas');
 
 var app = express();
+//conexion...
 
+//view
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+//app.use('/tareas', tareasRouter);
+
 
 module.exports = app;
