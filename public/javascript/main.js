f9d472ffb94ce29ab257f9f84408ba26f83b9bc9
Jorge <valencia12@uca.edu.sv>
2018-12-01 18:15:41 -0600

    nuevos cambios

---
+++ public/javascript/main.js
@@ -0,0 +1,54 @@
+window.onload = (e) = {
+    //app.init(e);
+}
+let app = {
+    
+}
+
+fetch('/users'),{
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers:{
+        'Content-type': 'application/json'
+    }
+}.then(res=>res.json(){
+
+})
+fetch('/users'),{
+    method: 'GET',
+    body: JSON.stringify(data),
+    headers:{
+        'Content-type': 'application/json'
+    }
+}.then(res=>res.json(){
+
+})
+
+fetch('/users'),{
+    method: 'DELETE',
+    body: JSON.stringify(data),
+    headers:{
+        'Content-type': 'application/json'
+    }
+}.then(res=>res.json(){
+
+})
+fetch('/users'),{
+    method: 'UPDATE',
+    body: JSON.stringify(data),
+    headers:{
+        'Content-type': 'application/json'
+    }
+}.then(res=>res.json(){
+
+})
+fetch('/users'),{
+    method: 'PUT',
+    body: JSON.stringify(data),
+    headers:{
+        'Content-type': 'application/json'
+    }
+}.then(res=>res.json(){
+
+})
+
